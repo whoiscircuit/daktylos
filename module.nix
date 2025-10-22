3ac1b3782b6e88c5271f761a11a9055a6a8ca525
circuit <oxcl@proton.me>
2025-10-22 10:11:40 +0330

    fix syntax issues in module.nix

--- module.nix
+++ module.nix
@@ -1,7 +1,7 @@
 {config,lib, pkgs,...}: 
 let
   cfg = config.services.hidrosis;
-  hidrosis = pkg.callPackage ./default.nix {pkgs};
+  hidrosis = pkg.callPackage ./default.nix {inherit pkgs; };
 in
 {
     options.services.hidrosis = {
