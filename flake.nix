c7520f7019591f87a33524dc6b513fc845f67a5e
circuit <oxcl@proton.me>
2025-10-22 10:06:22 +0330

    add nixos module for hidrosis

--- flake.nix
+++ flake.nix
@@ -11,12 +11,13 @@
     pkgs = import nixpkgs {
       inherit system;
     };
-    hidrosis = import ./default.nix pkgs;
+    hidrosis = import ./default.nix {pkgs};
   in rec {
     packages.${system}.default = hidrosis;
     devShell = pkgs.mkShell {inputsFrom = hidrosis;};
     overlays.default = final: prev: {
       inherit hidrosis;
     };
+    nixosModules.default = import ./module.nix;
   };
 }
---
+++ module.nix
@@ -0,0 +1,22 @@
+{config,lib, pkgs,...}: 
+let
+  cfg = config.services.hidrosis;
+  hidrosis = pkg.callPackage ./default.nix {pkgs};
+in
+{
+    options.services.hidrosis = {
+        enable = lib.mkEnableOption "Enable hidrosis service and udev rule";
+    };
+
+    config = lib.mkIf cfg.enable {
+        services.udev.packages = [ hidrosis ];
+
+        systemd.user.services.hidrosis = {
+            unitConfig = {
+                SourcePath = "${hidrosis}/lib/systemd/user/hidrosis.service";
+            };
+        };
+        
+        environment.systemPackages = [ hidrosis ];
+    };
+}
