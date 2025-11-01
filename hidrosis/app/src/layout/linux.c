d7a16dffc7a462d0548d9b149f6dbfa4904638fc
circuit <oxcl@proton.me>
2025-11-01 10:15:48 +0330

    fix old code and change wayland to hyprland

--- hidrosis/app/src/layout/linux.c
+++ hidrosis/app/src/layout/linux.c
@@ -5,7 +5,14 @@
 keyboard_layout_t get_keyboard_layout(void) {
     const char *val = getenv("WAYLAND_DISPLAY");
     if (val) {
-        return wayland_get_keyboard_layout();
+        const char *compositor = getenv("XDG_CURRENT_DESKTOP");
+        if (compositor && strcmp(compositor, "Hyprland") == 0) {
+            return hyprland_get_keyboard_layout();
+        }
+        else {
+            LOG_ERROR("Unsupported compositor: %s", compositor);
+            return LAYOUT_UNKNOWN;
+        }
     } else {
         return x11_get_keyboard_layout();
     }
